1d395dfe4533ea3c7291c75be96ba4e97ffadfea
Faychan <nor.iffat32@gmail.com>
2017-02-19 01:56:00 +0700

    - Final Task 3 Features

--- ASP_MVC_2/Controllers/HomeController.cs
+++ ASP_MVC_2/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using ASP_MVC_2.Security;
+using System.Web.Security;
+using ASP_MVC_2.Models.ViewModel;
+using ASP_MVC_2.Models.EntityManager;
 
 namespace ASP_MVC_2.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
@@ -16,13 +22,65 @@ namespace ASP_MVC_2.Controllers
         {
             return View();
         }
-
         [AuthorizeRole("Admin")]
-        public ActionResult AdminOnly() {
+        public ActionResult AdminOnly()
+        {
             return View();
         }
-        public ActionResult UnAuthorized(){
-           return View();
+
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+        [AuthorizeRole("Admin")]
+        public ActionResult ManageUserPartial(string status = "")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager UM = new UserManager();
+                UserDataView UDV = UM.GetUserDataView(loginName);
+
+                string message = string.Empty;
+                if (status.Equals("update"))
+                    message = "Update Successful";
+                else if (status.Equals("delete"))
+                    message = "Delete Successful";
+
+                ViewBag.Message = message;
+
+                return PartialView(UDV);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+        [AuthorizeRole("Admin")]
+        public ActionResult UpdateUserData(int userID, string loginName, string password,
+string firstName, string lastName, string gender, int roleID = 0)
+        {
+            UserProfileView UPV = new UserProfileView();
+            UPV.SYSUserID = userID;
+            UPV.LoginName = loginName;
+            UPV.Password = password;
+            UPV.FirstName = firstName;
+            UPV.LastName = lastName;
+            UPV.Gender = gender;
+
+            if (roleID > 0)
+                UPV.LOOKUPRoleID = roleID;
+
+            UserManager UM = new UserManager();
+            UM.UpdateUserAccount(UPV);
+
+            return Json(new { success = true });
+        }
+        [AuthorizeRole("Admin")]
+        public ActionResult DeleteUser(int userID)
+        {
+            UserManager UM = new UserManager();
+            UM.DeleteUser(userID);
+            return Json(new { success = true });
         }
     }
+
 }
